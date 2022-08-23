dff244c026283886b1ac2fb87ea3597341059913
Awet11_Tesfay <hailegb2021@gmail.com>
2022-08-23 12:26:55 -0700

    Task14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 strty = ""
 for i in reversed(range(97, 123)):
-     if (i % 2) == 0:
-         strty += chr(i)
-     else:
-         strty += chr(i-32)
+    if (i % 2) == 0:
+        strtmp += chr(i)
+    else:
+        strty += chr(i-32)
 print("{}".format(strty), end="")
