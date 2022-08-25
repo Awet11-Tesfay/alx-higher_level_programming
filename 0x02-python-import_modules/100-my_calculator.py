808b62f24f0dff568487bebf14c8d84bc463594a
Awet11_Tesfay <hailegb2021@gmail.com>
2022-08-25 13:27:04 -0700

    Task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,23 +2,23 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    arg = sys.arg[1:]
-    arg_count = len(arg)
+    argv = sys.argv[1:]
+    argv_count = len(arg)
     operators = ["+", "-", "*", "/"]
-    if arg_count != 3:
+    if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.arg[2] not in operators:
+    elif sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        a = int(sys.arg[1])
-        b = int(sys.arg[3])
-        if sys.arg[2] is "+":
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.arg[2] is "-":
+        elif sys.argv[2] is "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.arg[2] is "*":
+        elif sys.argv[2] is "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.arg[2] is "/":
+        elif sys.argv[2] is "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
