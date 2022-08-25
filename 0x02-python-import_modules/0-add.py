d708557ee7d4566c64bc7a71873eb25c4d633cde
Awet11_Tesfay <hailegb2021@gmail.com>
2022-08-25 07:18:07 -0700

    Task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 #0-add.py
-if _name__ ==" __main__":
+if _name__ == "__main__":
     '''sum of 1 and 2 '''
     from add_0.py import add
     a = 1
     b = 2
-    print("{} + {} = {}".format((a,b),add(a,b,))
+    print("{} + {} = {}".format(a,b,add(a,b,)))
 
