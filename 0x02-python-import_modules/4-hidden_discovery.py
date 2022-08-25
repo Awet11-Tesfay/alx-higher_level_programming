968df65042ce5c88caf70831c7264b0d75e4195c
Awet11_Tesfay <hailegb2021@gmail.com>
2022-08-25 11:36:24 -0700

    Task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     import hidden_4
-    if a[:2] != "__":
-        print(a)
+    for n in dir(hidden_4):
+        if n[0] != "_" and n[1] != "_":
+            print("{}".format(n))
