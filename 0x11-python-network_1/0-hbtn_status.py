35b363c897da424facd60955fb0ad49d41943e84
Awet11-Tesfay <hailegb2021@gmail.com>
2022-12-16 21:19:29 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         con = response.read()
 
         print("Body response:")
-        print(f"\t- type: {}".format(type(con)))
-        print(f"\t- content: {}".format{con})
-        print(f"\t- utf8 content: {con.decode('utf-8')}")
+        print("\t- type: {}".format(type(con)))
+        print("\t- content: {}".format(con))
+        print("\t- utf8 content:".format(con.decode("utf-8")))
