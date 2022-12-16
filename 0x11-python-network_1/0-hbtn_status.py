599514f949f3270776dbfcee506c30ccb8b0fa52
Awet11-Tesfay <hailegb2021@gmail.com>
2022-12-16 20:25:15 +0300

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""python script that fetches from https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import urllib.request
+    from urllib.request import Request, urlopen
+    req = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(req) as response:
+        con = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(con)))
+        print("\t- content: {}".format(con))
+        print(f"\t- utf8 content: {con.decode('utf-8')}")
